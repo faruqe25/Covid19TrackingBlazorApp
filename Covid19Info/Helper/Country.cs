be5e11d70c3e1dcc612d10f9e617a9f7904d4ea6
faruqe25 <faruqe25@gmail.com>
2020-10-20 00:49:27 +0600

    Completed Blazor App

---
+++ Covid19Info/Helper/Country.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Covid19Info.Helper 
+{
+    public class CountryInfo
+    {
+        public List<Country> countries { get; set; }
+        
+
+    }
+    public class Country 
+    {
+        public string name { get; set; }
+        public string iso2 { get; set; }
+        public string iso3 { get; set; }
+    }
+}
---
+++ Covid19Info/Helper/CountryWise.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Covid19Info.Helper 
+{
+    public class CountryWise
+    {
+        public Confirmed confirmed { get; set; }
+        public Recovered recovered { get; set; }
+        public Deaths deaths { get; set; }
+        public DateTime lastUpdate { get; set; }
+        public string countryName { get; set; } 
+    }
+    public class Confirmed
+    {
+        public int value { get; set; }
+        public string detail { get; set; }
+    }
+
+    public class Recovered
+    {
+        public int value { get; set; }
+        public string detail { get; set; }
+    }
+
+    public class Deaths
+    {
+        public int value { get; set; }
+        public string detail { get; set; }
+    }
+
+   
+}
